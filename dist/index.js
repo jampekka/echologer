b2e7a5f41f2345c8633f0ba29b27551586042d8a
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2024-03-21 16:33:14 +0200

    Base scaffolding

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+(() => {
+  var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __commonJS = (cb, mod) => function __require() {
+    return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+  };
+
+  // index.coffee
+  var require_echologer = __commonJS({
+    "index.coffee"(exports) {
+      (function() {
+        console.log("Jou2");
+      }).call(exports);
+    }
+  });
+  require_echologer();
+})();
+//# sourceMappingURL=index.js.map
